d25d8c058ddec0e695d2fe1d30339521c6788ba5
EMEA\naikd <durvesh.naik@travelex.com>
2019-06-29 13:23:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+const port = process.env.port || 3500;
+
+//Static folder to keep app static files
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+io.on("connection", (client) => {
+    console.log("connected...");
+    client.emit("acknowledge", { message: "Connection established" });
+
+    client.on("msgToServer", (chattername, message) => {
+        console.log(chattername +" says: " + message);
+        client.emit("msgToClient", 'Me',message);
+        client.broadcast.emit("msgToClient",chattername,message);
+    });
+});
+
+server.listen(port, () => {
+    console.log("socket server started at port : " + port);
+});
